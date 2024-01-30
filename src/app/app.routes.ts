a0d3591cf7690838af6fe6e9b764cc07e5e9f555
etud user <etud@port-hp-elitebook650G9-23>
2024-01-30 18:45:14 +0100

    ajout interface creation puzzle

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { PuzzleComputerComponent } from './components/puzzle-computer.component';
 import { PuzzleMoviesComponent } from './components/puzzle-movies.component';
+import { CreatePuzzleComponent } from './components/create-puzzle.component';
 
 export const routes: Routes = [
   { path: 'computer-puzzle', component: PuzzleComputerComponent },
-  { path: 'movies-puzzle', component: PuzzleMoviesComponent }
+  { path: 'movies-puzzle', component: PuzzleMoviesComponent },
+  { path: 'create-puzzle', component: CreatePuzzleComponent }
+
 ];
---
+++ src/app/components/create-puzzle.component.ts
@@ -0,0 +1,140 @@
+import { Component } from '@angular/core';
+import {NgClass, NgForOf} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-create-puzzle',
+  standalone: true,
+  imports: [
+    NgClass,
+    NgForOf,
+    FormsModule,
+    CommonModule
+  ],
+  templateUrl: '../templates/create-puzzle.component.html',
+  styleUrl: '../css/create-puzzle.component.css'
+})
+export class CreatePuzzleComponent {
+  clickedOnce: { [key: string]: boolean } = {};
+  clickedTwice: { [key: string]: boolean } = {};
+  annule: { [key: string]: boolean } = {};
+  propagates: { [key: string]: boolean } = {};
+  nb_propagates: { [key: string]: number } = {};
+
+  people: { name: string }[] = [{ name: '' }, { name: '' }, { name: '' }];
+  objects: { name: string }[] = [{ name: '' }, { name: '' }, { name: '' }];
+  places: { name: string }[] = [{ name: '' }, { name: '' }, { name: '' }];
+  showTable: boolean = false;
+  onSubmit() {
+    console.log('Nouvelles personnes :', this.people);
+    console.log('Nouveaux objets :', this.objects);
+    console.log('Nouveaux lieux :', this.places);
+
+    // Vous pouvez ajouter ici le code pour traiter les données (par exemple, les envoyer à un service)
+  }
+  cellClicked(name: string, pc: string) {
+    console.log(name);
+    console.log(pc);
+    if(this.propagates[name + pc] == false || this.propagates[name + pc] == undefined) {
+      if (this.clickedOnce[name + pc] == undefined && this.clickedTwice[name + pc] == undefined && this.annule[name + pc] == undefined) {
+        this.cellClickedOnce(name, pc);
+      } else if (this.clickedOnce[name + pc]) {
+        this.cellClickedTwice(name, pc);
+        this.propage(name, pc);
+      } else if (this.clickedTwice[name + pc]) {
+        this.cellAnnule(name, pc);
+        this.propage(name, pc);
+      } else if (this.annule[name + pc]) {
+        this.cellClickedOnce(name, pc);
+      }
+    }
+  }
+
+  propage(name: string, pc: string){
+    var var1: string[] = [];
+    var var2: string[] =[];
+    if (this.people.some(person => person.name == name)) {
+      this.people.forEach((m) => {if(m.name != name) {var1.push(m.name)}});
+    }else if (this.objects.some(obj => obj.name == name)) {
+      this.objects.forEach((o) => {if(o.name != name) {var1.push(o.name)}});
+    }else if (this.places.some(place => place.name == name)) {
+      this.places.forEach((pl) => {if(pl.name != name) {var1.push(pl.name)}});
+    }
+
+    if (this.people.some(person => person.name == pc)) {
+      this.people.forEach((m) => {if(m.name != pc) {var2.push(m.name)}});
+    }else if (this.objects.some(obj => obj.name == pc)) {
+      this.objects.forEach((o) => {if(o.name != pc) {var2.push(o.name)}});
+    }else if (this.places.some(place => place.name == pc)) {
+      this.places.forEach((pl) => {if(pl.name != pc) {var2.push(pl.name)}});
+    }
+    console.log(var1);
+    console.log(var2);
+    var1.forEach((v1 : string) : void => {
+      if (this.clickedTwice[name+pc]) {
+        //this.cellClickedOnce(v1, pc);
+        this.cellPropagate(v1,pc)
+      } else if(this.annule[name+pc]){
+        //if(this.clickedOnce[v1+pc]) {
+        this.cellReversePropagate(v1,pc)
+        //this.cellAnnule(v1, pc);
+        //}
+      }
+    })
+    var2.forEach((v2 : string) : void => {
+      if (this.clickedTwice[name+pc]) {
+        //this.cellClickedOnce(name, v2);
+        this.cellPropagate(name,v2);
+      }else if(this.annule[name+pc]) {
+        //if(this.clickedOnce[name+v2]) {
+        this.cellReversePropagate(name,v2);
+        //this.cellAnnule(name, v2);
+        //}
+      }
+    })
+
+  }
+  cellClickedOnce(name: string, pc: string) {
+    this.clickedOnce[name + pc] = true;
+    this.clickedTwice[name + pc] = false;
+    this.annule[name + pc] = false;
+  }
+
+  cellClickedTwice(name: string, pc: string) {
+    this.clickedOnce[name + pc] = false;
+    this.clickedTwice[name + pc] = true;
+    this.annule[name + pc] = false;
+  }
+
+  cellAnnule(name: string, pc: string) {
+    this.clickedOnce[name + pc] = false;
+    this.clickedTwice[name + pc] = false;
+    this.annule[name + pc] = true;
+  }
+
+  cellPropagate(name: string, pc: string) {
+
+    if((this.clickedTwice[name + pc]==false || this.clickedTwice[name + pc]==undefined)  && (this.clickedOnce[name + pc]==false || this.clickedOnce[name + pc]==undefined )) {
+      this.propagates[name + pc] = true;
+      if(this.nb_propagates[name + pc] == undefined)
+        this.nb_propagates[name + pc]=1;
+      else this.nb_propagates[name + pc]+=1;
+      console.log(this.nb_propagates[name + pc]);
+    }
+  }
+
+  cellReversePropagate(name: string, pc: string) {
+    if((this.clickedTwice[name + pc]==false || this.clickedTwice[name + pc]==undefined)  && (this.clickedOnce[name + pc]==false || this.clickedOnce[name + pc]==undefined )) {
+      if (this.nb_propagates[name + pc] == 1) {
+        this.propagates[name + pc] = false;
+        this.nb_propagates[name + pc] -=1;
+      } else if (this.nb_propagates[name + pc] > 1) {
+        this.nb_propagates[name + pc] -=1;
+      }
+    }
+  }
+
+  protected readonly Object = Object;
+}
+
